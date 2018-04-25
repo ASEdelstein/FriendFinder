e7ae5b6696e9ede372d1e3e182d3aad9e70df412
Anthony Edelstein <asedelstein@gmail.com>
2018-04-25 16:18:11 -0700

    file push for hw

---
+++ apiRoutes.js
@@ -0,0 +1,38 @@
+//This file needs to be able to get all matches and input new matches and return result
+var friends = require("../../app/data/friends")();
+
+module.exports = function(app) {
+    //Get all the friends	
+    app.get("/api/friends?", function(req, res) {
+        res.json(friends);
+    });
+
+    // Adding new survey info to the array
+    app.post("/api/new", function(req, res) {
+        var userData = req.body;
+        console.log(userData);
+
+        console.log(friends);
+        friends.push(userData);
+
+        //logic for comparing the arrays for differences
+        var bestmatch;
+        var bestscore = 999;
+
+        for (var i = 0; i < friends.length; i++) {
+            var score = 0;
+            for (var j = 0; j < 10; j++) {
+                var partialsum = Math.abs(friends[i].scores[j] - userData.scores[j]);
+                score += partialsum;
+            }
+            if (score < bestscore) {
+                bestscore = score;
+                bestmatch = i;
+            }
+        }
+
+        res.json(friends[bestmatch]);
+    });
+
+    return app;
+};
--- htmlRoutes.js
+++ htmlRoutes.js
@@ -1,1 +1,14 @@
+//creat variable that redirects user to home path or survey
+var path = require('path');
 
+module.exports = function(app) {
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../../app/public/home.html"));
+    });
+
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../../app/public/survey.html"));
+    });
+
+    return app;
+};
